e21a591fa0e065a09099ed1b7d28fa1a44ae7aef
Bmijin <bmj13465@jtts.co.kr>
2024-08-16 10:18:25 +0900

    객체지향프로그래밍 개념 및 예제

---
+++ src/oop1/ValueData.java
@@ -0,0 +1,11 @@
+package oop1;
+
+public class ValueData {
+    int value;
+
+    // 메서드도 함께 클래스에 정의
+    void add() {
+        value++;
+        System.out.println("숫자 증가 value = " + value);
+    }
+}
