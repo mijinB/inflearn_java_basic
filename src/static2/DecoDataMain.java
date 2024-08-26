139066539f74d01e3edea8c0404cd0b444f3ea5c
Bmijin <bmj13465@jtts.co.kr>
2024-08-26 14:51:11 +0900

    static 메서드 개념2 & import static

--- src/static2/DecoDataMain.java
+++ src/static2/DecoDataMain.java
@@ -1,5 +1,8 @@
 package static2;
 
+// import static static2.DecoData.staticCall;
+// import static static2.DecoData.*;
+
 public class DecoDataMain {
     public static void main(String[] args) {
         System.out.println("1. 정적 호출");
@@ -13,6 +16,12 @@ public class DecoDataMain {
         DecoData data2 = new DecoData();
         data2.instanceCall();
 
-        DecoData.staticCall(data1);
+        // 추가
+        // 인스턴스를 통한 접근
+        DecoData data3 = new DecoData();
+        data3.staticCall();
+
+        // 클래스를 통한 접근
+        DecoData.staticCall();
     }
 }
