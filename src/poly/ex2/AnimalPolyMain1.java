4800caa2af719b14eadfa7c92b30606b4a0b0d93
Bmijin <bmj13465@jtts.co.kr>
2024-09-06 10:07:46 +0900

    다형성 활용2 (poly>ex1 -> poly>ex2)

---
+++ src/poly/ex2/AnimalPolyMain1.java
@@ -0,0 +1,20 @@
+package poly.ex2;
+
+public class AnimalPolyMain1 {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Cow cow = new Cow();
+
+        // 부모는 자식을 담을 수 있다. 업캐스팅
+        soundAnimal(dog);
+        soundAnimal(cat);
+        soundAnimal(cow);
+    }
+
+    private static void soundAnimal(Animal animal) {
+        System.out.println("동물 소리 테스트 시작");
+        animal.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+}
