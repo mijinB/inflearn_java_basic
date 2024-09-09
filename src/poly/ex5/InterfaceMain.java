ec3c0c575ce212b044e2d9645ee38fcc389178a6
Bmijin <bmj13465@jtts.co.kr>
2024-09-09 09:56:44 +0900

    인터페이스 개념 및 예제

---
+++ src/poly/ex5/InterfaceMain.java
@@ -0,0 +1,21 @@
+package poly.ex5;
+
+public class InterfaceMain {
+    public static void main(String[] args) {
+        // InterfaceAnimal interfaceAnimal = new InterfaceAnimal();     인터페이스 생성 불가
+
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        Cow cow = new Cow();
+
+        soundAnimal(dog);
+        soundAnimal(cat);
+        soundAnimal(cow);
+    }
+
+    private static void soundAnimal(InterfaceAnimal animal) {
+        System.out.println("동물 소리 테스트 시작");
+        animal.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+}
